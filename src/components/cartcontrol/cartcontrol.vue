152f0fce56c5011d88e36bb0b9751d4a44266aa4
jack484 <1006395325@qq.com>
2017-12-30 17:32:19 +0800

    init

--- src/components/cartcontrol/cartcontrol.vue
+++ src/components/cartcontrol/cartcontrol.vue
@@ -47,19 +47,16 @@
       transition: all 0.4s linear
       &.move-transition
         opacity: 1
-        transform: translate3d(0, 0, 0)
+        transform: translate3d(0, 0, 0) rotate(0deg)
       .inner
         display: inline-block
         line-height: 24px
         font-size: 24px
         color: rgb(0, 160, 220)
         transition: all 0.4s linear
-        transform: rotate(0)
       &.move-enter,&.move-leave-to
         opacity: 0
-        transform: translate3d(24px, 0, 0)
-      .inner
-        transform: rotate(180deg)
+        transform: translate3d(24px, 0, 0) rotate(180deg)
     .cart-count
       display: inline-block
       vertical-align: top
